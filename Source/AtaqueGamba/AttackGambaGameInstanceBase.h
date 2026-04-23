5eb5e92abbb1c9692a2adb979ad328d23ab8bf77
mariarapsan <131718516+maria6092@users.noreply.github.com>
2026-04-23 18:17:06 +0200

    ajustando bp character

---
+++ Source/AtaqueGamba/AttackGambaGameInstanceBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "AttackGambaGameInstanceBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ATAQUEGAMBA_API UAttackGambaGameInstanceBase : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
